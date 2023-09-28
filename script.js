a96e60ba9fd7353d4533fe0ade720a59e82912f7
Veerabramham <122279394+Veerabramha@users.noreply.github.com>
2023-09-28 11:05:58 +0530

    script.js

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ function convert(){
     resultEl.classList.add("result");
     if(inputEl.value === ""){
         alert("Please enter a temperatue.");
-        return;
+        return flase;
     }
 
     let temp = parseFloat(inputEl.value);
 
     if(isNaN(temp)){
         alert("Please enter a valid number for temperatue.");
-        return;
+        return false;
     }
 
     if(from.value === "celsius" && to.value === "fahrenheit"){
@@ -22,14 +22,37 @@ function convert(){
     }
 
     else if(from.value === "fahrenheit" && to.value === "celsius"){
-        resultEl.innerHTML = `${temp}°F is equal to ${(temp - 32) * 5/9}°C`;;
+        resultEl.innerHTML = `${temp}°F is equal to ${(temp - 32) * 5/9}°C`;
     }
 
-    else if (from.value == to.value){
+    else if (from.value === to.value){
         resultEl.innerHTML = `${temp}°${from.value.toUpperCase()} is equal to ${temp}°${to.value.toUpperCase()}`;
     } 
 
-    return false;
+    else if (from.value === "kelvin" && to.value === "celsius") {
+        resultEl.innerHTML = `${temp}K is equal to ${(temp - 273.15)}°C`;
+    } 
 
+    else if (from.value === "fahrenheit" && to.value === "kelvin") {
+        resultEl.innerHTML = `${temp}°F is equal to ${(temp + 459.67) * 5/9}K`;
+    } 
+    
+    else if (from.value === "kelvin" && to.value === "fahrenheit") {
+        resultEl.innerHTML = `${temp}K is equal to ${(temp * 9/5) - 459.67}°F`;
+    } 
+    
+    else if (from.value === to.value) {
+        resultEl.innerHTML = `${temp}°${fromValue.toUpperCase()} is equal to ${temp}°${to.value.toUpperCase()}`;
+    }
+
+
+    return false;
 }
 
+function clearFields(){
+    inputEl.value = "";
+    from.value = "none";
+    to.value = "none";
+    resultEl.innerHTML = "";
+    resultEl.classList.remove("result");
+}
